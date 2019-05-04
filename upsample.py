636b7ecfc1463937b13cb515a29ae411a5301ab8
Haicheng Charles Zhao <czhao39@gmail.com>
2019-05-04 01:09:52 -0400

    Upsample training set to approximately half-and-half

--- upsample.py
+++ upsample.py
@@ -4,7 +4,7 @@ import numpy as np
 DATA_PATH = "data/"
 TRAIN_FILE = "exoTrain.csv"
 TEST_FILE = "exoTest.csv"
-UPSAMPLE_MULT = 10  # must be a whole number
+UPSAMPLE_MULT = 136  # must be a whole number
 
 
 train_data = np.loadtxt("{}{}".format(DATA_PATH, TRAIN_FILE), skiprows=1, delimiter=',')
@@ -13,6 +13,7 @@ print("x_train shape:", x_train.shape)
 y_train = (train_data[:, 0] - 1).flatten().astype(int)
 print("y_train freqs:")
 print(np.unique(y_train, return_counts=True))
+print()
 
 x_train_exo = x_train[y_train == 1]
 x_train = np.vstack((x_train, np.repeat(x_train_exo, UPSAMPLE_MULT - 1, axis=0)))
@@ -21,11 +22,13 @@ y_train_exo = np.ones(x_train_exo.shape[0])
 y_train = np.concatenate((y_train, np.repeat(y_train_exo, UPSAMPLE_MULT - 1, axis=0)))
 print("upsampled y_train freqs:")
 print(np.unique(y_train, return_counts=True))
+print()
 
 print("Writing upsampled train files...")
 filename_base = TRAIN_FILE[:TRAIN_FILE.rindex(".")]
 np.save("{}{}Data_upsampled".format(DATA_PATH, filename_base), x_train)
 np.save("{}{}Labels_upsampled".format(DATA_PATH, filename_base), y_train)
+print()
 
 
 test_data = np.loadtxt("{}{}".format(DATA_PATH, TEST_FILE), skiprows=1, delimiter=',')
@@ -34,6 +37,7 @@ print("x_test shape:", x_test.shape)
 y_test = (test_data[:, 0] - 1).flatten().astype(int)
 print("y_test freqs:")
 print(np.unique(y_test, return_counts=True))
+print()
 
 print("Writing non-upsampled test files...")
 filename_base = TEST_FILE[:TEST_FILE.rindex(".")]
