5b52944160b193d2c0bef07dca4f0c2b688b34f4
Bilal Mukadam <bmukadam@princeton.edu>
2019-05-08 16:15:07 -0400

    Added code to smoothen/filter data before extracting features

--- extractFeatures.py
+++ extractFeatures.py
@@ -2,7 +2,8 @@ import numpy as np
 import pywt
 from numpy import genfromtxt
 import os
-
+from astropy.stats import BoxLeastSquares
+from scipy.signal import medfilt
 
 
 def getFeatures(signal):
@@ -26,7 +27,28 @@ def getFeatures(signal):
 
 	return [a, b, c, d, wt1, wt2, wt3, wt4]
 
-
+stat_names = ["depth", "depth_odd", "depth_even", "depth_half", "depth_phased"]
+def get_bls_features(x, t):
+    features = []
+    # print(x[0])
+    for i in range(x.shape[0]):
+        # adapted from http://docs.astropy.org/en/stable/stats/bls.html#peak-statistics
+        bls = BoxLeastSquares(t, x[i])
+        periodogram = bls.autopower(40, minimum_n_transit=5)  # arg is the granularity of considered durations
+        max_power = np.argmax(periodogram.power)
+        stats = bls.compute_stats(periodogram.period[max_power],
+                                    periodogram.duration[max_power],
+                                    periodogram.transit_time[max_power])
+        # TODO: use dataframe?
+        features.append([stats[s][0] / stats[s][1] for s in stat_names])
+        # based on https://arxiv.org/pdf/astro-ph/0206099.pdf
+#         ratios.append(stats["depth"][0] / stats["depth"][1])  # depth over uncertainty
+        if (i + 1) % 10 == 0:
+            print(".", end="")
+        if (i + 1) % 500 == 0:
+            print()
+    print()
+    return np.array(features)
 
 trainData = np.load(os.path.join('data', 'exoTrainData_upsampled.npy'))
 
@@ -34,13 +56,31 @@ testData_filePath = os.path.join('data', 'exoTest.csv')
 testData = genfromtxt(testData_filePath, delimiter=',')
 testData = testData[1:,:] # get rid of labels
 
-allFeaturifiedData =[]
+filteredTrain = []
+for i in range(trainData.shape[0]):
+	rslt = medfilt(trainData[i],kernel_size=17)
+	filteredTrain.append(rslt)
+
+trainData = np.array(filteredTrain)
+
+filteredTest = []
+for i in range(testData.shape[0]):
+	rslt = medfilt(testData[i],kernel_size=17)
+	filteredTest.append(rslt)
+
+testData = np.array(filteredTest)
+
+allFeaturifiedData = []
 for i in range(trainData.shape[0]):
 	currSample = trainData[i][1:]
+
 	currExtractedFeatures = getFeatures(currSample)
 	allFeaturifiedData.append(currExtractedFeatures)
 
-np.save(os.path.join('data', 'featureExtractedTrainData.npy'), np.array(allFeaturifiedData))
+np.save(os.path.join('data', 'featureExtractedFilteredTrainData.npy'), np.array(allFeaturifiedData))
+# blsFeatures = get_bls_features(trainData, np.arange(trainData.shape[1]))
+# np.save(os.path.join('data', 'blsFilteredTrainData.npy'), np.array(blsFeatures))
+
 
 allFeaturifiedData =[]
 for i in range(testData.shape[0]):
@@ -48,4 +88,7 @@ for i in range(testData.shape[0]):
 	currExtractedFeatures = getFeatures(currSample)
 	allFeaturifiedData.append(currExtractedFeatures)
 
-np.save(os.path.join('data', 'featureExtractedTestData.npy'), np.array(allFeaturifiedData))
+np.save(os.path.join('data', 'featureExtractedFilteredTestData.npy'), np.array(allFeaturifiedData))
+
+# blsFeatures = get_bls_features(testData, np.arange(testData.shape[1]))
+# np.save(os.path.join('data', 'blsFilteredTestData.npy'), np.array(blsFeatures))
